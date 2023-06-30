cb31590fe8382753b110dd4b8206eed1283ed61d
pgtrungg <pgtrungg@gmail.com>
2023-06-30 12:14:30 +0700

    Update JsonToTxt.java

--- src/tools/JsonToTxt.java
+++ src/tools/JsonToTxt.java
@@ -8,33 +8,36 @@ import com.google.gson.JsonObject;
 import java.io.*;
 
 public class JsonToTxt {
-    public static void convert(String jsonFilePath, String txtFilePath) throws IOException{
+    public static void convert(String jsonFilePath, String txtFilePath) throws IOException {
         Gson gson = new Gson();
-        FileReader fileReader = new FileReader(jsonFilePath);
+        try (FileReader fileReader = new FileReader(jsonFilePath)) {
 
-        JsonObject jsonObject = gson.fromJson(fileReader, JsonObject.class);
+            JsonObject jsonObject = gson.fromJson(fileReader, JsonObject.class);
 
-        // Overall results
-        JsonObject statsObject = jsonObject
-                .getAsJsonObject("data")
-                .getAsJsonObject("attributes")
-                .getAsJsonObject("last_analysis_stats");
+            // Overall results
+            JsonObject statsObject = jsonObject
+                    .getAsJsonObject("data")
+                    .getAsJsonObject("attributes")
+                    .getAsJsonObject("last_analysis_stats");
 
-        // Detail results of different antivirus tools
-        JsonObject resultsObject = jsonObject
-                .getAsJsonObject("data")
-                .getAsJsonObject("attributes")
-                .getAsJsonObject("last_analysis_results");
+            // Detail results of different antivirus tools
+            JsonObject resultsObject = jsonObject
+                    .getAsJsonObject("data")
+                    .getAsJsonObject("attributes")
+                    .getAsJsonObject("last_analysis_results");
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter(txtFilePath));
-        for (String engineName : resultsObject.keySet()) {
-            JsonObject engineObject = resultsObject.getAsJsonObject(engineName);
-            JsonElement resultElement = engineObject.get("result");
-            JsonElement categoryElement = engineObject.get("category");
-            String resultValue = resultElement.toString();
-            String categoryValue = categoryElement.toString();
-            writer.write("Engine name@@ " + engineName + ", Category@@ " + categoryValue + ", Result@@ " + resultValue + "\n");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(txtFilePath));
+            for (String engineName : resultsObject.keySet()) {
+                JsonObject engineObject = resultsObject.getAsJsonObject(engineName);
+                JsonElement resultElement = engineObject.get("result");
+                JsonElement categoryElement = engineObject.get("category");
+                String resultValue = resultElement.toString();
+                String categoryValue = categoryElement.toString();
+                writer.write("Engine name@@ " + engineName + ", Category@@ " + categoryValue + ", Result@@ " + resultValue + "\n");
+            }
+            writer.close();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        writer.close();
     }
 }
